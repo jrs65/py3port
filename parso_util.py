a6b3da1da8ecfed86088ffade6f5f02bf618f145
Richard Shaw <richard@phas.ubc.ca>
2018-12-23 01:24:29 +0000

    Initial import.

---
+++ parso_util.py
@@ -0,0 +1,301 @@
+# Python 2/3 compatibility
+# pylint: disable=unused-import, redefined-builtin, no-name-in-module
+# noqa: F401
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from future.builtins import (ascii, bytes, chr, dict, filter, hex, input,
+                             int, map, next, oct, open, pow, range, round,
+                             str, super, zip)
+from future.builtins.disabled import (apply, cmp, coerce, execfile, file, long,
+                                      raw_input, reduce, reload, unicode,
+                                      xrange, StandardError)
+
+import parso
+import wrapt
+
+
+class ParsoProxy(wrapt.ObjectProxy):
+    """A thin wrapper around a parso node.
+    """
+
+    def __init__(self, wrapped):
+        super(ParsoProxy, self).__init__(wrapped)
+
+        if hasattr(wrapped, 'children'):
+            self._self_achildren = wrapped.children
+        else:
+            self._self_achildren = None
+
+    @property
+    def achildren(self):
+        return self._self_achildren
+
+    @achildren.setter
+    def achildren(self, value):
+        self._self_achildren = value
+
+
+def trim_power(node, parent):
+    """Remove the last entry from the power. If only one entry remains,
+    return only it.
+    """
+
+    if not node.type == 'power':
+        raise ValueError('Node type is not power')
+
+    if len(node.children) > 2:
+        trimmed_node = TempPower('power', node.children[:-1])
+        trimmed_node.parent = parent
+    else:
+        trimmed_node = node.children[0]
+
+    return trimmed_node
+
+
+class TempPower(object):
+    """Duck-typed power node which doesn't reset the parents."""
+
+    def __init__(self, type_, children):
+        self.children = children
+        self.type = type_
+        self.parent = None
+
+    def full(self):
+        node = parso.python.tree.PythonNode(self.type, self.children)
+        node.parent = self.parent
+        return node
+
+class AugmentedNode(object):
+
+    def __init__(self, node):
+
+        self.node = node
+        self.parent = node.parent
+
+
+class FuncCall(AugmentedNode):
+    """Easy wrapper around a function call.
+    """
+
+    def __init__(self, node):
+
+        super(FuncCall, self).__init__(node)
+
+        self.func = augment(trim_power(node, self))
+
+        trailer_node = node.children[-1]
+        if len(trailer_node.children) > 2:
+            # Has arguments...
+            if trailer_node.children[1].type == 'arglist':
+                self.arguments = [augment(arg) for arg in trailer_node.children[1].children
+                                  if arg.type != 'operator' or arg.value != ',']
+            else:
+                self.arguments = [augment(trailer_node.children[1])]
+        else:
+            self.arguments = []
+
+        self.achildren = [self.func] + self.arguments
+
+    @classmethod
+    def matches(cls, node):
+
+        if node.type != 'power':
+            return False
+
+        trailer_node = node.children[-1]
+
+        if trailer_node.type != 'trailer':
+            return False
+
+        if (trailer_node.children[0].type != 'operator' or
+                trailer_node.children[0].value != '('):
+            return False
+
+        if (trailer_node.children[-1].type != 'operator' or
+                trailer_node.children[-1].value != ')'):
+            return False
+
+        return True
+
+
+class Attribute(AugmentedNode):
+    """Easy wrapper around an Attribute."""
+
+    def __init__(self, node):
+
+        super(Attribute, self).__init__(node)
+
+        self.value = augment(trim_power(node, self))
+        self.attr = augment(node.children[-1].children[1])
+
+        self.achildren = [self.value, self.attr]
+
+
+    @classmethod
+    def matches(cls, node):
+
+        if node.type != 'power':
+            return False
+
+        trailer_node = node.children[-1]
+
+        if trailer_node.type != 'trailer':
+            return False
+
+        if (trailer_node.children[0].type != 'operator' or
+                trailer_node.children[0].value != '.'):
+            return False
+
+        return True
+
+
+class Subscript(AugmentedNode):
+    """Easy wrapper around subscripting."""
+
+    def __init__(self, node):
+
+        super(Subscript, self).__init__(node)
+
+        self.value = augment(trim_power(node, self))
+
+        trailer_node = node.children[-1]
+        if trailer_node.children[1].type == 'subscriptlist':
+            self.subscripts = [augment(arg) for arg in trailer_node.children[1].children
+                               if arg.type != 'operator' or arg.value != ',']
+        else:
+            self.subscripts = [augment(trailer_node.children[1])]
+
+        self.achildren = [self.value] + self.subscripts
+
+
+    @classmethod
+    def matches(cls, node):
+
+        if node.type != 'power':
+            return False
+
+        trailer_node = node.children[-1]
+
+        if trailer_node.type != 'trailer':
+            return False
+
+        if (trailer_node.children[0].type != 'operator' or
+                trailer_node.children[0].value != '['):
+            return False
+
+        if (trailer_node.children[-1].type != 'operator' or
+                trailer_node.children[-1].value != ']'):
+            return False
+
+        return True
+
+
+def augment(tree):
+    """An iterator that walks over a parso tree.
+
+    Parameters
+    ----------
+    tree : parso node
+        Root of tree (or subtree) to walk.
+
+    Yields
+    ------
+    node : parso node
+        Node from subtree. Order is complicated.
+    """
+
+    augtypes = [FuncCall, Attribute, Subscript]
+
+    # Walk tree yielding nodes
+
+    for type_ in augtypes:
+        if type_.matches(tree):
+            return type_(tree)
+
+    proxynode = ParsoProxy(tree)
+
+    if hasattr(tree, 'children'):
+        proxynode.achildren = [augment(child) for child in proxynode.children]
+
+    return proxynode
+
+
+def awalk(tree):
+    """An iterator that walks over a parso tree.
+
+    Parameters
+    ----------
+    tree : parso node
+        Root of tree (or subtree) to walk.
+
+    Yields
+    ------
+    node : parso node
+        Node from subtree. Order is complicated.
+    """
+
+    stack = [tree]
+
+    # Walk tree yielding nodes
+    while stack:
+        node = stack.pop()
+
+        yield node
+
+        if node.achildren:
+            for child in node.achildren[::-1]:
+                stack.append(child)
+
+
+def pwalk(tree):
+    """An iterator that walks over a parso tree.
+
+    Parameters
+    ----------
+    tree : parso node
+        Root of tree (or subtree) to walk.
+
+    Yields
+    ------
+    node : parso node
+        Node from subtree. Order is complicated.
+    """
+
+    stack = [tree]
+
+    # Walk tree yielding nodes
+    while stack:
+        node = stack.pop()
+
+        yield node
+
+        if hasattr(node, 'children'):
+            for child in node.children[::-1]:
+                stack.append(child)
+
+
+def find_ancestor(node, ancestor_list):
+    """See if any item in ancestor_list is an ancestor of node.
+
+    Parameters
+    ----------
+    node : parso node
+        Node to start the search from.
+    parent_list : list of nodes
+        Ancestor to search for.
+
+    Returns
+    -------
+    ancestor_node : parso node
+        The first ancestor found. Return None if not found.
+    """
+    while node:
+        if node in ancestor_list:
+            return node
+        node = node.parent
+    return None
+
+
+def is_float(node):
+    """Return true if node represents a float."""
+    return node.type == 'number' and '.' in node.value
---
+++ py3port.py
@@ -0,0 +1,376 @@
+# Python 2/3 compatibility
+# pylint: disable=unused-import, redefined-builtin, no-name-in-module
+# noqa: F401
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from future.builtins import (ascii, bytes, chr, dict, filter, hex, input,
+                             int, map, next, oct, open, pow, range, round,
+                             str, super, zip)
+from future.builtins.disabled import (apply, cmp, coerce, execfile, file, long,
+                                      raw_input, reduce, reload, unicode,
+                                      xrange, StandardError)
+
+import subprocess
+import sys
+import os
+import parso
+import parso_util
+import click
+
+
+def context_tree(tree, node, filename, num=3, style=None):
+    """Print some context around node.
+
+    Parameters
+    ----------
+    tree : parso node
+        Full source tree.
+    node : parso node
+        Node to give context around.
+    filename : string
+        Name of file to diplay.
+    num : int
+        Number of lines of context.
+    style : dict
+        Styling of nodes. Should be a mapping from parso node to a dictionary
+        of parameters given to `click.style`.
+    """
+
+
+    # Add a default style into the dict
+    style_dict = {None: {'fg': 'white'}}
+    if style:
+        style_dict.update(style)
+
+    # Find the start node
+    lineno = node.start_pos[0]
+    start_line = max(0, lineno - num)
+    print_node = node.get_previous_leaf()
+    while print_node.line > start_line:
+        print_node = print_node.get_previous_leaf()
+
+    # Printer header
+    click.secho("==== %s : %i ====" % (filename, lineno))
+
+    # Print (styled) nodes until we reach either the end of file, or have printed enough lines
+    while print_node.type != 'endmarker' and print_node.line < lineno + num:
+        node_style = style_dict[parso_util.find_ancestor(print_node, style_dict.keys())]
+        click.echo(click.style(print_node.get_code(), **node_style), nl=False)
+        print_node = print_node.get_next_leaf()
+
+
+
+def process_div(tree, filename):
+    """Find division operators, prompt for transform, and modify tree.
+
+    Detect any trivial float divisions (and preserve them). Any others are
+    prompted to see which type they should be.
+    """
+    for node in parso_util.pwalk(tree):
+        if node.type == 'operator' and node.value == '/':
+
+            # Print out the context around the operator
+            click.clear()
+            context_tree(tree, node, filename, num=8,
+                         style={
+                             node: {'fg':'red'},
+                             node.parent: {'fg': 'bright_white', 'bold': True}
+                         }
+            )
+            click.echo()
+
+
+            a = node.get_previous_sibling()
+            b = node.get_next_sibling()
+            if parso_util.is_float(a) or parso_util.is_float(b):
+                click.echo("Found trivial float division\n")
+                continue
+
+            click.echo("Options:")
+            click.echo("[F]: Floating point division")
+            click.echo("[I]: Integer division using the floor division operator")
+            div_type = click.prompt("Division type?", type=click.Choice(["F", "I"]))
+
+            if div_type == 'I':
+                node.value = '//'
+
+
+def process_iterview(tree, filename):
+    """Find items/keys/values that can be used in an iterator context.
+
+    This replaces them with the viewitems versions which are correctly
+    processed by futurize. Only does anything within the target of a for loop
+    and in list/dict comprehensions.
+    """
+    transform_dict = {
+        'items': 'viewitems',
+        'iteritems': 'viewitems',
+        'keys': 'viewkeys',
+        'iterkeys': 'viewkeys',
+        'values': 'viewvalues',
+        'itervalues': 'viewvalues'
+    }
+
+    # Find item and iteritems calls in for loops, and list,dict comprehensions
+    for node in parso_util.pwalk(tree):
+        if node.type in ['for_stmt', 'comp_for']:
+            iterable = parso_util.augment(node.children[3])
+
+            if not (isinstance(iterable, parso_util.FuncCall) and
+                    isinstance(iterable.func, parso_util.Attribute)):
+                    continue
+
+            last_call = iterable.func.attr
+
+            if not last_call.value in transform_dict:
+                continue
+
+            last_call.value = transform_dict[last_call.value]
+
+            context_tree(tree, node, filename, num=4,
+                         style={
+                             last_call: {'fg':'red'},
+                             iterable.node: {'fg': 'bright_white', 'bold': True}
+                         }
+            )
+
+
+def process_inkeys(tree, filename):
+    """Fix the in .keys() anti-pattern
+
+    Not strictly a Python 2->3 issue, but it does make the code uglier once
+    converted.
+    """
+    for node in parso_util.pwalk(tree):
+        if node.type != 'comparison':
+            continue
+
+        comp_op = node.children[1]
+
+        if not ((comp_op.type == 'keyword' and comp_op.value == 'in') or
+                (comp_op.type == 'comp_op' and
+                 comp_op.children[1].type == 'keyword' and
+                 comp_op.children[1].value == 'in')):
+            continue
+
+        target = parso_util.augment(node.children[2])
+
+        if not (isinstance(target, parso_util.FuncCall) and
+                isinstance(target.func, parso_util.Attribute)):
+            continue
+
+        last_call = target.func.attr
+
+        if last_call.value != 'keys':
+            continue
+
+        new_target = parso_util.trim_power(parso_util.trim_power(target.node, node), node)
+        if isinstance(new_target, parso_util.TempPower):
+            new_target = new_target.full()
+
+        node.children[2] = new_target
+
+    click.secho("Fixing 'a in x.keys()' antipattern.", bold=True)
+    click.echo("\n\n")
+
+
+def process_octal(tree, filename):
+    """Remove unnecessary octal literals.
+
+    There's a bunch of octal numbers that creep in from people trying ot
+    zero pad integers (WRONG!) in datetimes. This gets rid of them.
+    """
+    for node in parso_util.pwalk(tree):
+        if node.type != 'number':
+            continue
+
+        # Check if is an octal number
+        if not (len(node.value) > 1 and '.' not in node.value and
+                    node.value[0] == '0'):
+            continue
+
+        context_tree(
+            tree, node, filename, num=2,
+            style={
+                node: {'fg':'red'},
+                node.parent: {'fg': 'bright_white', 'bold': True}
+            }
+        )
+
+        if node.parent.type == 'arglist':
+            func = node.parent.parent.get_previous_sibling()
+            func = func.children[1] if func.type == 'trailer' else func
+
+            if func.value in ['datetime', 'date']:
+                click.echo("Correcting octal number within datetime\n")
+                node.value = node.value[1:]
+
+
+def process_imports(tree, filename):
+    """Add Python 2/3 imports.
+
+    Here we remove any existing `__future__` or `builtins` imports and
+    replace them with a full set of imports. We carefully transfer over any
+    comments and docstrings at the file head.
+    """
+
+    import_txt = """### Start Python 2/3 compatibility
+# pylint: disable=unused-import, redefined-builtin, no-name-in-module
+# noqa: F401
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from future.builtins import (ascii, bytes, chr, dict, filter, hex, input,
+                             int, map, next, oct, open, pow, range, round,
+                             str, super, zip)
+from future.builtins.disabled import (apply, cmp, coerce, execfile, file, long,
+                                      raw_input, reduce, reload, unicode,
+                                      xrange, StandardError)
+### End Python 2/3 compatibility
+
+"""
+
+
+    if tree.type != 'file_input':
+        raise ValueError("This only works at the module level")
+
+    def is_future_builtin(node):
+        return ((node.type == 'import_from' and len(node.get_from_names()) == 1 and
+                 node.get_from_names()[0].value in ['__future__', 'builtins']) or
+                (node.type =='simple_stmt' and is_future_builtin(node.children[0])))
+
+    # Remove all redundant __future__ and builtins imports
+    orig_first = tree.children[0]  # Needed to sort out header comments below
+    tree.children = [n for n in tree.children if not is_future_builtin(n)]
+
+    def has_docstring(node):
+        return (node.children[0] != 'simple_stmt' and
+                node.children[0].children[0].type == 'string')
+
+    pos = 1 if has_docstring(tree) else 0
+
+    import_tree = parso.parse(import_txt)
+
+    for oi, stmt in enumerate(import_tree.children):
+        tree.children.insert(pos + oi, stmt)
+
+    # We may have removed the first node which would have contained any
+    # comments at the start of the file, to get around this we will transfer
+    # over the prefix if we need to.
+    if is_future_builtin(orig_first):
+        leaf0 = tree.children[0].get_first_leaf()
+        leaf0.prefix = orig_first.get_first_leaf().prefix + leaf0.prefix
+
+    click.secho("Adding imports.", bold=True)
+    click.echo("\n\n")
+
+
+def process_int(tree, filename):
+    """Replace `int` when used as a datatype.
+
+    Because of the overriden builtin we can't use `int` as a datatype
+    argument for numpy (and we should probably never have been doing so).
+    This function replaces them with `np.int`.V
+    """
+
+    for node in parso_util.pwalk(tree):
+
+        # Find all uses of `int`
+        if not (node.type == 'name' and node.value == 'int'):
+            continue
+
+        # Replace where used as a dtype=int keyword argument
+        if (node.parent.type == 'argument' and
+                node.get_previous_sibling().type == 'operator' and
+                node.get_previous_sibling().value == '=' and
+                node.parent.children[0].type == 'name' and
+                node.parent.children[0].value == 'dtype'):
+            node.value = 'np.int'
+
+        # Replace where used solely in an astype
+        elif (node.parent.type == 'trailer' and
+                  node.get_previous_sibling().type == 'operator'):
+
+            func = parso_util.augment(node.parent.parent)
+
+            if not (isinstance(func, parso_util.FuncCall) and
+                    isinstance(func.func, parso_util.Attribute)):
+                continue
+
+            last_call = func.func.attr
+
+            if last_call.value != 'astype':
+                continue
+
+            node.value = 'np.int'
+
+
+def preprocess(filename):
+
+    with open(filename, 'r') as fh:
+        tree = parso.parse(fh.read())
+
+    if tree.children[0].type == 'endmarker':
+        return
+
+    process_div(tree, filename)
+    process_inkeys(tree, filename)
+    process_iterview(tree, filename)
+    process_octal(tree, filename)
+
+    with open(filename, 'w') as fh:
+        fh.write(tree.get_code())
+
+
+def postprocess(filename):
+
+    with open(filename, 'r') as fh:
+        tree = parso.parse(fh.read())
+
+    if tree.children[0].type == 'endmarker':
+        return
+
+    process_imports(tree, filename)
+    process_int(tree, filename)
+
+    with open(filename, 'w') as fh:
+        fh.write(tree.get_code())
+
+
+
+def process(filename):
+
+    click.secho("########## %s ###########" % filename, bold=True)
+
+
+    click.secho("Preprocessing:")
+    preprocess(filename)
+
+    # Call futurize
+    click.secho("Calling futurize:")
+    subprocess.check_call(("futurize -0 -u -x libfuturize.fixes.fix_division_safe -w %s" % filename).split())
+
+    click.secho("Post processing:")
+    postprocess(filename)
+
+
+@click.command()
+@click.argument('files', nargs=-1)
+def main(files):
+    if not len(files):
+        files = []
+
+        for dirpath, _, filenames in os.walk('.'):
+
+            for name in filenames:
+
+                if not os.path.splitext(name)[1] == '.py':
+                    continue
+
+                files.append(os.path.join(dirpath, name))
+
+    for filename in files:
+        process(filename)
+
+
+if __name__ == '__main__':
+    main()   # pylint: disable=E1120
